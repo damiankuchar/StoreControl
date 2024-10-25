78c9edcbe86fc8352682e3a58e2d5f83013041d8
Damian Kucharski <damiankuchar1@gmail.com>
2024-10-25 17:01:56 +0200

    Added permission query tests

---
+++ tests/StoreControl.Application.IntegrationTests/Features/PermissionFeatures/Queries/GetAllPermissionsQueryHandler/GetAllPermissionsQueryHandlerFixture.cs
@@ -0,0 +1,31 @@
+﻿using StoreControl.Application.IntegrationTests.Abstractions;
+using StoreControl.Domain.Entities;
+
+namespace StoreControl.Application.IntegrationTests.Features.PermissionFeatures.Queries.GetAllPermissionsQueryHandler
+{
+    public class GetAllPermissionsQueryHandlerFixture : BaseIntegrationTest
+    {
+        public GetAllPermissionsQueryHandlerFixture(IntegrationTestWebAppFactory factory)
+            : base(factory)
+        {
+        }
+
+        protected async Task SeedDatabase()
+        {
+            var permissions = new List<Permission>()
+            {
+                new()
+                {
+                    Name = "TestPermission1",
+                },
+                new() 
+                {
+                    Name = "TestPermission2",
+                }
+            };
+
+            await DbContext.Permissions.AddRangeAsync(permissions);
+            await DbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ tests/StoreControl.Application.IntegrationTests/Features/PermissionFeatures/Queries/GetAllPermissionsQueryHandler/GetAllPermissionsQueryHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using StoreControl.Application.Features.PermissionsFeatures.Queries.GetAllPermisions;
+using StoreControl.Application.IntegrationTests.Abstractions;
+
+namespace StoreControl.Application.IntegrationTests.Features.PermissionFeatures.Queries.GetAllPermissionsQueryHandler
+{
+    public class GetAllPermissionsQueryHandlerTests : GetAllPermissionsQueryHandlerFixture
+    {
+        public GetAllPermissionsQueryHandlerTests(IntegrationTestWebAppFactory factory)
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnAllPermissions_WhenPermissionsExist()
+        {
+            // Arrange
+            var query = new GetAllPermisionsQuery();
+            await SeedDatabase();
+
+            // Act
+            var result = await Mediator.Send(query);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().HaveCount(2);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnEmptyList_WhenNoPermissionsExist()
+        {
+            // Arrange
+            var query = new GetAllPermisionsQuery();
+
+            // Act
+            var result = await Mediator.Send(query);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeEmpty();
+        }
+    }
+}
